246542581391b1d9a06bfe681058aebd4fad7f5a
Gustavo Bento <gustavobentoteles379@gmail.com>
2026-06-24 17:59:33 -0300

    add: melhoria no visual da janela do autocomplete e mudança de tema

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,66 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    appearance = {
+      kind_icons = {
+        Text = "󰉿",
+        Method = "󰆧",
+        Function = "󰊕",
+        Constructor = "󰡱",
+
+        Field = "󰜢",
+        Variable = "󰀫",
+        Property = "󰜢",
+
+        Class = "󰠱",
+        Interface = "",
+        Module = "󰆧",
+
+        Unit = "",
+        Value = "󰎠",
+        Enum = "",
+        EnumMember = "",
+
+        Keyword = "󰌋",
+        Snippet = "",
+
+        Color = "󰏘",
+        File = "󰈙",
+        Folder = "󰉋",
+
+        Reference = "󰈇",
+        Constant = "󰏿",
+        Struct = "󰙅",
+        Event = "",
+        Operator = "󰆕",
+        TypeParameter = "󰊄",
+      },
+    },
+
+    completion = {
+      menu = {
+        border = "rounded",
+        draw = {
+          columns = {
+            { "kind_icon", gap = 1 },
+            { "label", gap = 1 },
+            { "source_name" },
+          },
+        },
+      },
+      documentation = {
+        auto_show = true,
+        window = {
+          border = "rounded",
+        },
+      },
+      ghost_text = {
+        enabled = true,
+      },
+    },
+
+    sources = {
+      default = { "lsp", "path", "buffer", "snippets" },
+    },
+  },
+}
